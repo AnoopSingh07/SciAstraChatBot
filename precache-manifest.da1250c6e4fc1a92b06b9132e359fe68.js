7af236c536fbebf4fdabed75ca6d982b8e084864
anoopsingh_1 <as14072002@gmail.com>
2024-01-02 23:37:23 +0530

    Updates

---
+++ precache-manifest.da1250c6e4fc1a92b06b9132e359fe68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad66aa349fd81a43012635b4da86a538",
+    "url": "/SciAstraChatBot/index.html"
+  },
+  {
+    "revision": "da847d9fdbdfbb5845a5",
+    "url": "/SciAstraChatBot/static/css/main.1a2391f7.chunk.css"
+  },
+  {
+    "revision": "bcd88c52d2f4a849ab82",
+    "url": "/SciAstraChatBot/static/js/2.0330136a.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/SciAstraChatBot/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "da847d9fdbdfbb5845a5",
+    "url": "/SciAstraChatBot/static/js/main.0710a757.chunk.js"
+  },
+  {
+    "revision": "3800877f78d76a14e9ec",
+    "url": "/SciAstraChatBot/static/js/runtime~main.020cffcf.js"
+  },
+  {
+    "revision": "c8624b5eaeae7c87111eac372c1e8309",
+    "url": "/SciAstraChatBot/static/media/chatbot-logo.c8624b5e.png"
+  }
+]);
